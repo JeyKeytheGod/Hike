399846e0b56c03ad373ffe5b54b6fee87faa1c92
Jakub Kaszuba <Mistrzuz2001@gmail.com>
2021-12-11 19:42:11 +0100

    Pick Up folder
    Pls dont crush

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField]
+    private Text pickUpText;
+
+    private bool pickUpAllowed;
+
+    private void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.F))
+            PickedUp();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("PlayerCharacter"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.Equals("PlayerCharacter"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+
+    void PickedUp()
+    {
+        Destroy(gameObject);
+    }
+
+}
